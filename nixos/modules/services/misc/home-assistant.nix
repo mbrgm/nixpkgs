4382d16093089dd850e87fe7568f66db5935123f
Jörg Thalheim <joerg@thalheim.io>
2020-05-29 22:55:38 +0100

    homeassistant: support for python_script automation
    Add an option to specify a list of python scripts to use in the
    python_scripts integration. Also see in the <link
    xlink:href="https://www.home-assistant.io/integrations/python_script">Homeassistant
    documentation</link>
    
    Co-authored-by: Marius Bergmann <marius@yeai.de>

--- nixos/modules/services/misc/home-assistant.nix
+++ nixos/modules/services/misc/home-assistant.nix
@@ -220,6 +220,17 @@ in {
       '';
     };
 
+    pythonScripts = mkOption {
+      #default = [];
+      #type = types.listOf types.path;
+      default = null;
+      type = types.nullOr types.path;
+      description = ''
+        List of python scripts to use in the <literal>python_scripts</literal> integration.
+        Also see in the <link xlink:href="https://www.home-assistant.io/integrations/python_script">Homeassistant documentation</link>
+      '';
+    };
+
     openFirewall = mkOption {
       default = false;
       type = types.bool;
@@ -230,9 +241,16 @@ in {
   config = mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
 
+    systemd.tmpfiles.rules = mkIf (cfg.pythonScripts != null) [
+      "L+ ${cfg.configDir}/python_scripts - - - - ${cfg.pythonScripts}"
+    ];
+
+    services.home-assistant.config.python_script = mkIf (cfg.pythonScripts != null) {};
+
     systemd.services.home-assistant = {
       description = "Home Assistant";
       after = [ "network.target" ];
+      restartTriggers = [ cfg.pythonScripts ];
       preStart = optionalString (cfg.config != null) (if cfg.configWritable then ''
         cp --no-preserve=mode ${configFile} "${cfg.configDir}/configuration.yaml"
       '' else ''
